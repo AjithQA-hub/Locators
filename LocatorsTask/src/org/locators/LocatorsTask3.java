68dba54c7576baaf2381b3f608b2adc006849211
AJITH V <113958920+AjithQA-hub@users.noreply.github.com>
2026-03-11 09:21:57 +0530

    Update LocatorsTask3.java

--- LocatorsTask/src/org/locators/LocatorsTask3.java
+++ LocatorsTask/src/org/locators/LocatorsTask3.java
@@ -1,8 +1,6 @@
 package org.locators;
 
-//import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LocatorsTask3 {
@@ -23,3 +21,4 @@ public class LocatorsTask3 {
 	}
 
 }
+
