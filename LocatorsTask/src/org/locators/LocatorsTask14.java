44cf7d2bc6b35acf72d64daba305595132df73d9
AJITH V <113958920+AjithQA-hub@users.noreply.github.com>
2026-04-08 14:50:56 +0530

    Update LocatorsTask14.java

--- LocatorsTask/src/org/locators/LocatorsTask14.java
+++ LocatorsTask/src/org/locators/LocatorsTask14.java
@@ -9,11 +9,8 @@ public class LocatorsTask14 {
 	public static void main(String[] args) {
 		
         System.setProperty("webdriver.chrome.driver","C:\\Users\\AJITH\\eclipse-workspace\\Locators1\\Drivers\\chromedriver.exe");
-		
 		WebDriver driver=new ChromeDriver();
-		
 		driver.get("https://www.google.com/");
-		
 		driver.manage().window().maximize();
 		
 		WebElement search = driver.findElement(By.xpath("//button[@type='submit']"));
@@ -21,8 +18,5 @@ public class LocatorsTask14 {
         
         WebElement search1= driver.findElement(By.xpath("//input[@type='text']"));
         search1.sendKeys("Inmakes learning hub");
-		
-		
 	}
-
 }
