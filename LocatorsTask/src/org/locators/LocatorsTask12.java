a57b3287e94a0e63aa7d45afb455e8d95e62c22a
AJITH V <113958920+AjithQA-hub@users.noreply.github.com>
2026-04-08 14:50:30 +0530

    Update LocatorsTask12.java

--- LocatorsTask/src/org/locators/LocatorsTask12.java
+++ LocatorsTask/src/org/locators/LocatorsTask12.java
@@ -8,18 +8,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class LocatorsTask12 {
 	public static void main(String[] args) {
 		
-
         System.setProperty("webdriver.chrome.driver","C:\\Users\\AJITH\\eclipse-workspace\\Locators1\\Drivers\\chromedriver.exe");
-		
 		WebDriver driver=new ChromeDriver();
-		
 		driver.get("https://inmakesedu.com/about_us");
-		
 		driver.manage().window().maximize();
 		
 		WebElement inmakes = driver.findElement(By.xpath("//p[contains(text(),'Inmakes learning')]"));
 		String text= inmakes.getText();
 		System.out.println(text);
 	}
-
 }
