d03a6395608d19af5f89426abb1de4417a365385
AJITH V <113958920+AjithQA-hub@users.noreply.github.com>
2026-04-08 14:51:55 +0530

    Update LocatorsTask4.java

--- LocatorsTask/src/org/locators/LocatorsTask4.java
+++ LocatorsTask/src/org/locators/LocatorsTask4.java
@@ -9,22 +9,17 @@ public class LocatorsTask4 {
 	public static void main(String[] args) {
 		
         System.setProperty("webdriver.chrome.driver","C:\\Users\\AJITH\\eclipse-workspace\\Locators1\\Drivers\\chromedriver.exe");
-		
 		WebDriver driver=new ChromeDriver();
-		
 		driver.get("https://en-gb.facebook.com/");
-		
 		driver.manage().window().maximize();
-		
 		
 				WebElement email = driver.findElement(By.xpath("//input[@data-testid='royal_email']"));
 		        email.sendKeys("ajithkumarvam1@gmail.com");
 		        
 		        WebElement pswd = driver.findElement(By.xpath("//input[@data-testid='royal_pass']"));
 		        pswd.sendKeys("Ajith!123");
-		       
+
 		        WebElement logbtn = driver.findElement(By.xpath("//button[@type='submit']"));
 		        logbtn.click();
 	}
-
 }
