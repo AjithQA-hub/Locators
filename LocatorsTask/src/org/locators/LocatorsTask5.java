4b195d73b25638a60624c01fe0c6b571345460a6
AJITH V <113958920+AjithQA-hub@users.noreply.github.com>
2026-04-08 14:52:12 +0530

    Update LocatorsTask5.java

--- LocatorsTask/src/org/locators/LocatorsTask5.java
+++ LocatorsTask/src/org/locators/LocatorsTask5.java
@@ -8,12 +8,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class LocatorsTask5 {
 	public static void main(String[] args) {
 		
-       System.setProperty("webdriver.chrome.driver","C:\\Users\\AJITH\\eclipse-workspace\\Locators1\\Drivers\\chromedriver.exe");
-		
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\AJITH\\eclipse-workspace\\Locators1\\Drivers\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
-		
 		driver.get("https://demo.automationtesting.in/Register.html");
-		
 		driver.manage().window().maximize();
 		
 		WebElement Firstname= driver.findElement(By.xpath("//input[@type='text']"));
