2a01c44e04ae5c85d85f8a8d155dcc199017e861
AJITH V <113958920+AjithQA-hub@users.noreply.github.com>
2026-04-08 14:53:00 +0530

    Update LocatorsTask9.java

--- LocatorsTask/src/org/locators/LocatorsTask9.java
+++ LocatorsTask/src/org/locators/LocatorsTask9.java
@@ -10,16 +10,11 @@ public class LocatorsTask9 {
 	public static void main(String[] args) {
 		
         System.setProperty("webdriver.chrome.driver","C:\\Users\\AJITH\\eclipse-workspace\\Locators1\\Drivers\\chromedriver.exe");
-		
 		WebDriver driver=new ChromeDriver();
-		
 		driver.get("https://www.flipkart.com/");
-		
 		driver.manage().window().maximize();
 		
 		WebElement logbtn = driver.findElement(By.xpath("//button[@type='Login']"));
         logbtn.click();
-		
 	}
-
 }
